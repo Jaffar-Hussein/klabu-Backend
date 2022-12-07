4d91526ca28b60eab690a067f2ff40fc5e6327c3
Jaffar Hussein <jaffhussein@icloud.com>
2022-12-07 20:17:36 +0300

    ft:recipes and comments- created this two models

---
+++ app/models/comment.rb
@@ -0,0 +1,3 @@
+class Comment < ApplicationRecord
+    belongs_to :recipes
+end
---
+++ db/migrate/20221207165103_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :description
+      t.string :ingredients
+      t.string :direction
+      t.string :picture
+
+      t.timestamps
+    end
+  end
+end
