ed562169e0ff9920ab3ed157138ceda32adfa2a3
Jaffar Hussein <jaffhussein@icloud.com>
2022-12-08 12:16:39 +0300

    ft:added auth configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   resources :categories
   resources :comments
   resources :recipes
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
